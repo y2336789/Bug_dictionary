7db8fb6461faaef03341819d0a7febf275d04745
y2336789 <63240477+y2336789@users.noreply.github.com>
2021-10-09 22:02:21 +0900

    10_09
    지역별 작물들에 대한 병충 리스트 출력 설정(전염병은 추후 추가예정)

---
+++ app/app/src/main/java/com/example/myapplication/VirusAdapter.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class VirusAdapter extends RecyclerView.Adapter{
+    String virusKey = null;
+    ArrayList<Virus> viruses;
+    Context context;
+
+    public VirusAdapter(ArrayList<Virus> items, Context context) {
+        this.viruses = items;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView= LayoutInflater.from(context).inflate(R.layout.recycler_virus,parent,false);
+        VH vh= new VH(itemView);
+        return vh;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        VH vh= (VH)holder;
+
+        //현재번째(position) 아이템 얻어오기
+        Virus virus= viruses.get(position);
+        vh.sickName.setText(virus.getName());
+        vh.sickCropName.setText("피해작물 : "+virus.getTarget());
+        virusKey=virus.getVirusKey();
+
+        //이미지는 없을 수도 있음.
+        if(virus.getImgUrl()==null){
+            vh.sickIv.setVisibility(View.GONE);
+        }else{
+            vh.sickIv.setVisibility(View.VISIBLE);
+            Glide.with(context).load(virus.getImgUrl()).into(vh.sickIv);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return viruses.size();
+    }
+
+    class VH extends RecyclerView.ViewHolder{
+
+        TextView sickName, sickCropName;
+        ImageView sickIv;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+
+            sickName=itemView.findViewById(R.id.sickName);
+            sickCropName=itemView.findViewById(R.id.sickCropName);
+            sickIv=itemView.findViewById(R.id.sickIv);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String virusKey = viruses.get(getLayoutPosition()).getVirusKey();
+
+                    Intent intent = new Intent(context, VirusItemActivity.class);
+                    intent.putExtra("VirusKey", virusKey);
+                    context.startActivity(intent);
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -7,45 +7,66 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class DetailActivity extends AppCompatActivity {
-
+    public String key = "20218f5922b84a6b4691db8472132ececb19";
     private Intent intent;
     private String dist, link;
-    TextView tview;
+    private String this_name, target_name;
+    private BugAdapter adapter;
+    private VirusAdapter virusAdapter;
+    private ArrayList<Bug> bugs = new ArrayList<>();
+    ArrayList<Virus> virus = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private Button change_btn;
+    TextView tview, tv00;
     ImageButton imagbtn;
     Context context;
     Document doc = null;
     LinearLayout layout;
+    int num = 0;
+    int check = 0;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather);
+        recyclerView = findViewById(R.id.weather_recycler);
+        adapter = new BugAdapter(bugs, this);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
 
         tview = (TextView)findViewById(R.id.textView);
         imagbtn = (ImageButton)findViewById(R.id.imagebtn);
         layout = (LinearLayout)findViewById(R.id.layout);
         context = this;
+        tv00=(TextView)findViewById(R.id.tv00);
 
         intent= getIntent();
         dist = intent.getStringExtra("code");
@@ -57,6 +78,36 @@ public class DetailActivity extends AppCompatActivity {
                 R.drawable.crop14, R.drawable.crop15, R.drawable.crop16};
         Integer[] ID={-1};
 
+        change_btn = findViewById(R.id.weather_change_btn);
+        change_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num == 0) {
+                    num = 1;
+                    changeView();
+                    change_btn.setText("해충 리스트로 전환");
+                } else if (num == 1) {
+                    num = 0;
+                    originalView();
+                    change_btn.setText("병 리스트로 전환");
+                }
+            }
+        });
+
+//        int j;
+//        for(j=0;j<17;j++){
+//            imageView[j] = findViewById(cropID[j]);
+//
+//            if(j==0) {
+//                imageView[j].setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                       readRss("수박");
+//                    }
+//                });
+//            }
+//        }
+
         int i;
         for(i=0;i<7;i++){
             ImageButton img = new ImageButton(context);
@@ -66,12 +117,75 @@ public class DetailActivity extends AppCompatActivity {
             img.setPadding(20,20,20,20);
             img.setBackgroundColor(Color.argb(0,0,0,0));
             img.setImageResource(cropID[i]);
+
+            switch (i){
+                case 0:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            check = 0;
+                            readRss("배추");
+                        }
+                    });
+                    break;
+                case 1 :
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("파");
+                        }
+                    });
+                    break;
+                case 2:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("고추");
+                        }
+                    });
+                    break;
+                case 3:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("무");
+                        }
+                    });
+                    break;
+                case 4:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("딸기");
+                        }
+                    });
+                    break;
+                case 5:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("토마토");
+                        }
+                    });
+                    break;
+
+                case 6:
+                    img.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            readRss("호박");
+                        }
+                    });
+                    break;
+            }
             layout.addView(img);
         }
 
 
         //충북
         if(dist.indexOf("43") == 0){
+            tv00.setText("충북 날씨 조회");
+
             ImageButton img = new ImageButton(context);
 
             img.setLayoutParams(new ViewGroup.LayoutParams(350,220));
@@ -79,11 +193,21 @@ public class DetailActivity extends AppCompatActivity {
             img.setPadding(20,20,20,20);
             img.setBackgroundColor(Color.argb(0,0,0,0));
             img.setImageResource(cropID[8]);
+
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    readRss("블루베리");
+                }
+            });
+
             layout.addView(img);
         }
 
         //충남
         else if(dist.indexOf("44") == 0){
+            tv00.setText("충남 날씨 조회");
+
             for(i=7;i<12;i++){
                 ImageButton img = new ImageButton(context);
 
@@ -99,6 +223,8 @@ public class DetailActivity extends AppCompatActivity {
 
         //대전
         else if(dist.indexOf("30")==0){
+            tv00.setText("대전 날씨 조회");
+
             for(i=12;i<16;i++){
                 ImageButton img = new ImageButton(context);
 
@@ -114,6 +240,8 @@ public class DetailActivity extends AppCompatActivity {
 
         //세종
         else if(dist.indexOf("36")==0){
+            tv00.setText("세종 날씨 조회");
+
             for(i=15;i<17;i++){
                 ImageButton img = new ImageButton(context);
 
@@ -128,6 +256,121 @@ public class DetailActivity extends AppCompatActivity {
 
         GetXMLTask1 task = new GetXMLTask1();
         task.execute(link);
+    }
+    void readRss(String cropName) {
+        if (cropName.equals("블루베리")) {
+            this_name = cropName;
+            if (bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=블루베리&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if(cropName.equals("배추")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=배추&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("파")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=파&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("고추")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=고추&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("무")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=무&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("딸기")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=딸기&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("토마토")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=토마토&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else if(cropName.equals("호박")) {
+            this_name = cropName;
+            if(bugs != null) {
+                bugs.clear();
+                adapter.notifyDataSetChanged();
+                try {
+                    URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/service?cropName=호박&insectKorName=&apiKey="
+                            + key + "&serviceCode=SVC03&serviceCodeDetail=SVC07&displayCount=50&insectKey=");
+                    RssFeedTask task = new RssFeedTask();
+                    task.execute(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private class GetXMLTask1 extends AsyncTask<String, Void, Document> {
@@ -201,5 +444,104 @@ public class DetailActivity extends AppCompatActivity {
 
             super.onPostExecute(doc);
         }
+    }
+    class RssFeedTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+
+            try {
+                InputStream is = url.openStream();
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(is, "utf-8");
+                int eventType = xpp.getEventType();
+
+                Bug bug = null;
+                String tagName = null;
+
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    switch (eventType) {
+                        case XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            tagName = xpp.getName();
+
+                            if (tagName.equals("item")) {
+                                bug = new Bug();
+                            }else if(tagName.equals("insectKorName")){
+                                xpp.next();
+                                if(bug!=null) {
+                                    target_name = xpp.getText();
+                                    bug.setName(xpp.getText());
+                                }
+                            }else if(tagName.equals("thumbImg")){
+                                xpp.next();
+                                if(target_name.equals("끝동매미충") & (this_name.equals("논벼"))){
+                                    bug.setImgUrl("http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer.mo?uploadSpec=npms&uploadSubDirectory=/photo/spcs/&imageFileName=ZM1AR0080M001[20110106120000000]_wm.jpg");
+                                }
+                                else if(target_name.equals("기장테두리진딧물") & (this_name.equals("보리"))){
+                                    bug.setImgUrl("http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer.mo?uploadSpec=npms&uploadSubDirectory=/photo/spcs/&imageFileName=ZM1AJ0184M001[20110106120000000]_wm.jpg");
+                                }
+//                                else if(target_name.equals("끝동매미충") & (this_name.equals("논벼"))){
+//                                    bug.setImgUrl();
+//                                }
+                                else if(bug!=null) bug.setImgUrl(xpp.getText());
+                            }else if(tagName.equals("speciesName")){
+                                xpp.next();
+                                if(bug!=null) bug.setSpecies(xpp.getText());
+                            }else if(tagName.equals("cropName")){
+                                xpp.next();
+                                bug.setCropName(xpp.getText());
+                            }else if(tagName.equals("insectKey")){
+                                xpp.next();
+                                if(bug!=null) bug.setInsectKey(xpp.getText());
+                            }
+                            break;
+                        case XmlPullParser.TEXT:
+                            break;
+                        case XmlPullParser.END_TAG:
+                            tagName=xpp.getName();
+                            if(tagName.equals("item")){
+                                if(bug.getCropName().equals(this_name)){
+                                    bugs.add(bug);
+                                    bug=null;
+                                }
+                                publishProgress();
+                            }
+                            break;
+                    }
+                    eventType = xpp.next();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            }
+            return "파싱종료";
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            adapter.notifyItemInserted(bugs.size());
+        }
+    }
+    private void originalView(){
+//        adapter = new BugAdapter(bugs, this);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+    private void changeView(){
+        // 식량작물
+        virusAdapter = new VirusAdapter(virus, this);
+        recyclerView.setAdapter(virusAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
